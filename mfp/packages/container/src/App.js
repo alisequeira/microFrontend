29054edf09a7d8a8778bb66c80e856f4f3b90a53
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-30 16:30:26 -0400

    Adding a Loading Bar;

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+import Progress from "./components/Progress";
 import Header from "./components/Header";
+
+const marketingLazy = lazy(() => import("./components/MarketingApp"));
+const authLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -18,10 +20,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={authLazy} />
+              <Route path="/" component={marketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
