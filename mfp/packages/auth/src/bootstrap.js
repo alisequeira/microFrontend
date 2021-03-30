beb4ac344e0e382c4aa3f8240c03dc4657bbd58f
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-30 15:17:19 -0400

    Adding Signin and Signup form

---
+++ mfp/packages/auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devROOT = document.querySelector("#_auth-dev-root");
+  if (devROOT) {
+    mount(devROOT, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
