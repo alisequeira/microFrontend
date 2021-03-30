9714cae9dfe74727a0c87c62cfc4c7a16807d31f
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-30 13:35:35 -0400

    Using Browser History in isolation

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,22 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
   }
 
   ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devROOT = document.querySelector("#_marketing-dev-root");
   if (devROOT) {
-    mount(devROOT, {});
+    mount(devROOT, { defaultHistory: createBrowserHistory() });
   }
 }
 
